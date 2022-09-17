7435d82969d9ff12611d239ae1bb13fe915eec37
wl <weeee148@hotmail.com>
2022-09-17 13:49:43 +0800

    test from vs

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ app.use(session({
 	secret: crypto.randomBytes(32).toString('hex'),
 	store: sessionStore,
 	resave: false,
-	saveUninitialized: true,
+	saveUninitialized: false,
 	cookie: { maxAge: 1000 * 60 * 60 * 24 },
 }))
 
@@ -55,7 +55,13 @@ const authenticationMiddleware = async (req, res, next) => {
 const indexHandler = async (req, res) => {
     //return res.send(pug.renderFile('templates/index.pug'))
 	//req.session.userId = 'aaaa';
-	return res.send(pug.renderFile('views/home.pug'))
+    //session = req.session;
+	//console.log(req.session)
+	if(req.session.userId){
+		console.log("logged in")
+		return res.send(pug.renderFile('views/home.pug', {fn: ""}))
+	}
+	res.send(pug.renderFile('views/home.pug'))
 }
 
 const getLoginHandler = async (req, res) => {
@@ -67,27 +73,36 @@ const getRegisterHandler = async (req, res) => {
 }
 
 const postRegisterHandler = async (req, res) => {
-    console.log(req.body)
-	const { title, ln, fn, cntry, state, phone, email, dob_d, dob_m, dob_y, pass, cfm_pass, register } = req.body
-	console.log(title);
-	const rows = await query(`INSERT INTO users VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [email, pass, title, fn, ln, phone, "2000-7-04", cntry, state]);
+    //console.log(req.body)
+	const { title, ln, fn, country, state, phone, email, dob, pass, cfm_pass } = req.body
+	try{
+		const rows = await query(`INSERT INTO users VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [email, pass, title, fn, ln, phone, dob, country, state])
+		//res.status(200).send("Registration successful. Redirecting to home page...");
+		res.redirect("/");
+	}
+	catch (err){
+		//console.log('MY SQL ERROR', err);
+		console.log(err.sqlMessage);
+		res.send(pug.renderFile('views/register.pug', {err: err.sqlMessage}))
+	}
 	//return res.send(pug.renderFile('views/home.pug')
 }
 
 const postLoginHandler = async (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password)
-        return res.status(400).send({ message: 'Missing email or password' })
-
-    const rows = await query(`SELECT * FROM users WHERE email = ? AND password = ?`, [email, password])
-    if (rows.length === 0)
-        return res.status(401).send({ message: 'Invalid email or password' })
-
-    req.session.userId = rows[0].id
-    return res.status(200).send({ message: "Success" })
+	//console.log(req.body)
+	const { email, password } = req.body
+    if (!email || !password){}
+    const rows = await query(`SELECT * FROM users WHERE email = ? AND pass = ?`, [email, password])
+    if (rows.length === 0){
+        return res.status(401).send(pug.renderFile('views/login.pug', {err: "Wrong username or password"}))
+	}
+	req.session.userId = email
+	//console.log(req.session)
+    return res.redirect("/")
 }
 
 app.get('/', authenticationMiddleware, indexHandler)
 app.get('/login', authenticationMiddleware, getLoginHandler)
+app.post('/login', authenticationMiddleware, urlencodedParser, postLoginHandler)
 app.get('/register', authenticationMiddleware, getRegisterHandler)
 app.post('/register', authenticationMiddleware, urlencodedParser, postRegisterHandler)
